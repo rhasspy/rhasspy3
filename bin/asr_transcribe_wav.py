7a6c8ebf91c0aa6731787715a3bb9d5d23d68734
Michael Hansen <mike@rhasspy.org>
2023-02-02 14:12:35 -0600

    Clean up transcribe tool

--- bin/asr_transcribe_wav.py
+++ bin/asr_transcribe_wav.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python3
+"""Transcribes WAV audio into text."""
 import argparse
 import asyncio
 import io
+import json
 import logging
+import os
 import sys
 import wave
-from typing import Iterable, Optional
+from pathlib import Path
+from typing import Iterable
 
 from rhasspy3.asr import DOMAIN, Transcript
-from rhasspy3.audio import AudioChunk, AudioStart, AudioStop
+from rhasspy3.audio import (
+    DEFAULT_IN_CHANNELS,
+    DEFAULT_IN_RATE,
+    DEFAULT_IN_WIDTH,
+    AudioChunk,
+    AudioChunkConverter,
+    AudioStart,
+    AudioStop,
+)
 from rhasspy3.core import Rhasspy
 from rhasspy3.event import async_read_event, async_write_event
 from rhasspy3.program import create_process
 
-_LOGGER = logging.getLogger("transcribe_wav")
+_FILE = Path(__file__)
+_DIR = _FILE.parent
+_LOGGER = logging.getLogger(_FILE.stem)
 
 
 async def main() -> None:
@@ -21,17 +35,50 @@ async def main() -> None:
     parser.add_argument(
         "-c",
         "--config",
-        required=True,
+        default=_DIR.parent / "config",
         help="Configuration directory",
     )
-    parser.add_argument("-n", "--name", required=True, help="ASR program name")
-    parser.add_argument("wav", nargs="*", help="Path(s) to WAV file(s)")
+    parser.add_argument(
+        "-p", "--pipeline", default="default", help="Name of pipeline to use"
+    )
+    parser.add_argument(
+        "--asr-program", help="Name of ASR program to use (overrides pipeline)"
+    )
+    #
+    parser.add_argument(
+        "--rate", type=int, default=DEFAULT_IN_RATE, help="Sample rate (hertz)"
+    )
+    parser.add_argument(
+        "--width", type=int, default=DEFAULT_IN_WIDTH, help="Sample width (bytes)"
+    )
+    parser.add_argument(
+        "--channels", type=int, default=DEFAULT_IN_CHANNELS, help="Sample channel count"
+    )
+    #
+    parser.add_argument("wav", nargs="*", help="Path to WAV file(s)")
+    parser.add_argument(
+        "--output-json", action="store_true", help="Outputs JSON instead of text"
+    )
+    parser.add_argument(
+        "--debug", action="store_true", help="Print DEBUG messages to console"
+    )
     args = parser.parse_args()
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
 
     rhasspy = Rhasspy.load(args.config)
+    asr_program = args.asr_program
+    pipeline = rhasspy.config.pipelines.get(args.pipeline)
 
+    if not asr_program:
+        assert pipeline is not None, f"No pipline named {args.pipeline}"
+        asr_program = pipeline.asr
+
+    assert asr_program, "No asr program"
+
+    # Transcribe WAV file(s)
     for wav_bytes in get_wav_bytes(args):
+        converter = AudioChunkConverter(args.rate, args.width, args.channels)
+
         with io.BytesIO(wav_bytes) as wav_io:
             with wave.open(wav_io, "rb") as wav_file:
                 rate = wav_file.getframerate()
@@ -43,22 +90,27 @@ async def main() -> None:
                 timestamp = int(wav_seconds * 1_000)
                 audio_bytes = wav_file.readframes(num_frames)
 
-        async with (await create_process(rhasspy, DOMAIN, args.name)) as asr_proc:
+                chunk = AudioChunk(rate, width, channels, audio_bytes, timestamp=0)
+                chunk = converter.convert(chunk)
+
+        async with (await create_process(rhasspy, DOMAIN, asr_program)) as asr_proc:
             assert asr_proc.stdin is not None
             assert asr_proc.stdout is not None
 
+            # Write audio
             await async_write_event(
                 AudioStart(rate, width, channels, timestamp=0).event(), asr_proc.stdin
             )
             await async_write_event(
-                AudioChunk(rate, width, channels, audio_bytes, timestamp=0).event(),
+                chunk.event(),
                 asr_proc.stdin,
             )
             await async_write_event(
                 AudioStop(timestamp=timestamp).event(), asr_proc.stdin
             )
 
-            transcript: Optional[Transcript] = None
+            # Read transcript
+            transcript = Transcript(text="")
             while True:
                 event = await async_read_event(asr_proc.stdout)
                 if event is None:
@@ -68,11 +120,13 @@ async def main() -> None:
                     transcript = Transcript.from_event(event)
                     break
 
-            if transcript is not None:
-                print(transcript.text)
+            if args.output_json:
+                # JSON output
+                json.dump(transcript.event().data, sys.stdout)
+                print("", flush=True)
             else:
-                # No transcript
-                print("")
+                # Text output
+                print(transcript.text or "", flush=True)
 
 
 def get_wav_bytes(args: argparse.Namespace) -> Iterable[bytes]:
@@ -83,6 +137,9 @@ def get_wav_bytes(args: argparse.Namespace) -> Iterable[bytes]:
                 yield wav_file.read()
     else:
         # WAV on stdin
+        if os.isatty(sys.stdin.fileno()):
+            print("Reading WAV audio from stdin", file=sys.stderr)
+
         yield sys.stdin.buffer.read()
 
 
