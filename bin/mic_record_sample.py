9169be8850d4b4513ef7734ce3f56d9a925d93c4
Michael Hansen <mike@rhasspy.org>
2023-02-08 21:24:26 -0600

    Finish mic record sample

--- bin/mic_record_sample.py
+++ bin/mic_record_sample.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
+"""Record a spoken audio sample to a WAV file."""
 import argparse
 import asyncio
 import logging
+import wave
+from collections import deque
 from pathlib import Path
+from typing import Deque
 
-from rhasspy3.audio import AudioChunk, AudioStop
+from rhasspy3.audio import AudioChunk
 from rhasspy3.core import Rhasspy
 from rhasspy3.event import async_read_event, async_write_event
 from rhasspy3.mic import DOMAIN as MIC_DOMAIN
 from rhasspy3.program import create_process
-from rhasspy3.vad import DOMAIN as VAD_DOMAIN, VoiceStarted, VoiceStopped
+from rhasspy3.vad import DOMAIN as VAD_DOMAIN
+from rhasspy3.vad import VoiceStarted, VoiceStopped
 
 _FILE = Path(__file__)
 _DIR = _FILE.parent
@@ -18,6 +23,8 @@ _LOGGER = logging.getLogger(_FILE.stem)
 
 async def main() -> None:
     parser = argparse.ArgumentParser()
+    parser.add_argument("wav_file", nargs="+", help="Path to WAV file(s) to write")
+    #
     parser.add_argument(
         "-c",
         "--config",
@@ -34,18 +41,20 @@ async def main() -> None:
         "--vad-program", help="Name of vad program to use (overrides pipeline)"
     )
     #
-    # parser.add_argument(
-    #     "--keep-chunks-before",
-    #     type=int,
-    #     default=0,
-    #     help="Audio chunks to keep before voice starts",
-    # )
-    # parser.add_argument(
-    #     "--keep-chunks-after",
-    #     type=int,
-    #     default=0,
-    #     help="Audio chunks to keep after voice ends",
-    # )
+    parser.add_argument(
+        "-b",
+        "--keep-chunks-before",
+        type=int,
+        default=0,
+        help="Audio chunks to keep before voice starts",
+    )
+    parser.add_argument(
+        "-a",
+        "--keep-chunks-after",
+        type=int,
+        default=0,
+        help="Audio chunks to keep after voice ends",
+    )
     #
     parser.add_argument(
         "--debug", action="store_true", help="Print DEBUG messages to console"
@@ -70,56 +79,98 @@ async def main() -> None:
 
     assert vad_program, "No vad program"
 
-    async with (await create_process(rhasspy, MIC_DOMAIN, mic_program)) as mic_proc, (
-        await create_process(rhasspy, VAD_DOMAIN, vad_program)
-    ) as vad_proc:
-        assert mic_proc.stdout is not None
-        assert vad_proc.stdin is not None
-        assert vad_proc.stdout is not None
+    for wav_path in args.wav_file:
+        wav_file: wave.Wave_write = wave.open(wav_path, "wb")
+        with wav_file:
+            before_chunks: Deque[AudioChunk] = deque(maxlen=args.keep_chunks_before)
+            is_first_chunk = True
 
-        mic_task = asyncio.create_task(async_read_event(mic_proc.stdout))
-        vad_task = asyncio.create_task(async_read_event(vad_proc.stdout))
-        pending = {mic_task, vad_task}
+            async with (
+                await create_process(rhasspy, MIC_DOMAIN, mic_program)
+            ) as mic_proc, (
+                await create_process(rhasspy, VAD_DOMAIN, vad_program)
+            ) as vad_proc:
+                assert mic_proc.stdout is not None
+                assert vad_proc.stdin is not None
+                assert vad_proc.stdout is not None
 
-        in_command = False
-        while True:
-            done, pending = await asyncio.wait(
-                pending, return_when=asyncio.FIRST_COMPLETED
-            )
-            if mic_task in done:
-                mic_event = mic_task.result()
-                if mic_event is None:
-                    break
-
-                # Process chunk
-                if AudioChunk.is_type(mic_event.type):
-                    if in_command:
-                        await async_write_event(mic_event, vad_proc.stdin)
-                    else:
-                        await async_write_event(mic_event, vad_proc.stdin)
-
-                # Next chunk
+                _LOGGER.info("Recording %s", wav_path)
                 mic_task = asyncio.create_task(async_read_event(mic_proc.stdout))
-                pending.add(mic_task)
-
-            if vad_task in done:
-                vad_event = vad_task.result()
-                if vad_event is None:
-                    break
-
-                if VoiceStarted.is_type(vad_event.type):
-                    if not in_command:
-                        # Start of voice command
-                        in_command = True
-                        _LOGGER.debug("segment: speaking started")
-                elif VoiceStopped.is_type(vad_event.type):
-                    # End of voice command
-                    _LOGGER.debug("segment: speaking ended")
-                    break
-
-                # Next VAD event
                 vad_task = asyncio.create_task(async_read_event(vad_proc.stdout))
-                pending.add(vad_task)
+                pending = {mic_task, vad_task}
+
+                before_command = True
+                while True:
+                    done, pending = await asyncio.wait(
+                        pending, return_when=asyncio.FIRST_COMPLETED
+                    )
+                    if mic_task in done:
+                        mic_event = mic_task.result()
+                        if mic_event is None:
+                            break
+
+                        # Process chunk
+                        if AudioChunk.is_type(mic_event.type):
+                            chunk = AudioChunk.from_event(mic_event)
+                            if is_first_chunk:
+                                is_first_chunk = False
+                                wav_file.setframerate(chunk.rate)
+                                wav_file.setsampwidth(chunk.width)
+                                wav_file.setnchannels(chunk.channels)
+
+                            await async_write_event(mic_event, vad_proc.stdin)
+                            if before_command:
+                                before_chunks.append(chunk)
+                            else:
+                                wav_file.writeframes(chunk.audio)
+
+                        # Next chunk
+                        mic_task = asyncio.create_task(
+                            async_read_event(mic_proc.stdout)
+                        )
+                        pending.add(mic_task)
+
+                    if vad_task in done:
+                        vad_event = vad_task.result()
+                        if vad_event is None:
+                            break
+
+                        if VoiceStarted.is_type(vad_event.type):
+                            if before_command:
+                                # Start of voice command
+                                while before_chunks:
+                                    chunk = before_chunks.popleft()
+                                    wav_file.writeframes(chunk.audio)
+
+                                before_command = False
+                                _LOGGER.info("Speaking started")
+                        elif VoiceStopped.is_type(vad_event.type):
+                            # End of voice command
+                            _LOGGER.info("Speaking ended")
+                            break
+
+                        # Next VAD event
+                        vad_task = asyncio.create_task(
+                            async_read_event(vad_proc.stdout)
+                        )
+                        pending.add(vad_task)
+
+                # After chunks
+                num_chunks_left = args.keep_chunks_after
+                while num_chunks_left > 0:
+                    mic_event = await mic_task
+                    if mic_event is None:
+                        break
+
+                    if AudioChunk.is_type(mic_event.type):
+                        chunk = AudioChunk.from_event(mic_event)
+                        wav_file.writeframes(chunk.audio)
+                        num_chunks_left -= 1
+
+                    if num_chunks_left > 0:
+                        mic_task = asyncio.create_task(
+                            async_read_event(mic_proc.stdout)
+                        )
 
 
 if __name__ == "__main__":
