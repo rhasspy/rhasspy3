5f8d2ac852163f57c96af3728ff6cc19fbbe8da3
Michael Hansen <mike@rhasspy.org>
2023-02-08 22:45:24 -0600

    Add stream2stream

--- rhasspy3_http_api/pipeline.py
+++ rhasspy3_http_api/pipeline.py
@@ -2,7 +2,7 @@ import argparse
 import asyncio
 import logging
 from collections import deque
-from typing import Optional, Deque
+from typing import Optional, Deque, Union
 
 from quart import request, Quart, Response, jsonify, websocket
 
@@ -15,15 +15,16 @@ from rhasspy3.audio import (
     DEFAULT_OUT_WIDTH,
 )
 from rhasspy3.asr import DOMAIN as ASR_DOMAIN, Transcript, transcribe_stream
-from rhasspy3.audio import AudioChunkConverter
+from rhasspy3.audio import AudioChunkConverter, AudioChunk, AudioStop
 from rhasspy3.core import Rhasspy
 from rhasspy3.config import PipelineConfig
-from rhasspy3.event import Event, async_read_event
+from rhasspy3.event import Event, async_read_event, async_write_event
 from rhasspy3.mic import DOMAIN as MIC_DOMAIN
+from rhasspy3.handle import Handled, NotHandled, handle
 from rhasspy3.program import create_process
 from rhasspy3.intent import recognize, Intent
 from rhasspy3.wake import detect
-from rhasspy3.vad import segment
+from rhasspy3.vad import segment, DOMAIN as VAD_DOMAIN, VoiceStopped, VoiceStarted
 from rhasspy3.tts import synthesize_stream
 from rhasspy3.pipeline import run as run_pipeline, StopAfterDomain
 
@@ -54,16 +55,16 @@ def add_pipeline(
         )
 
         _LOGGER.debug(
-            "listen-for-command:"
-            + "mic=%s,"
-            + "wake=%s,"
-            + "vad=%s,"
-            + "asr=%s,"
-            + "intent=%s,"
-            + "handle=%s,"
-            + "tts=%s,"
-            + "snd=%s,"
-            + "stop_after=%s",
+            "listen-for-command: "
+            "mic=%s,"
+            "wake=%s,"
+            "vad=%s,"
+            "asr=%s,"
+            "intent=%s,"
+            "handle=%s,"
+            "tts=%s,"
+            "snd=%s,"
+            "stop_after=%s",
             mic_program,
             wake_program,
             vad_program,
@@ -88,20 +89,29 @@ def add_pipeline(
             handle_program=handle_program,
             tts_program=tts_program,
             snd_program=snd_program,
-            stop_after=stop_after,
+            stop_after=StopAfterDomain(stop_after) if args.stop_after else None,
         )
 
         return jsonify(pipeline_result.to_dict())
 
     @app.websocket("/api/stream-to-stream")
-    async def api_stream_to_stream():
-        asr_program = websocket.args.get("asr_program", pipeline.asr)
-        assert asr_program, "Missing program for asr"
-        _LOGGER.debug("speech-to-text: asr=%s", asr_program)
+    async def ws_api_stream_to_stream() -> None:
+        used_pipeline = pipeline
+        pipeline_name = websocket.args.get("pipeline")
+        if pipeline_name:
+            used_pipeline = rhasspy.config.pipelines[pipeline_name]
 
-        tts_program = websocket.args.get("tts_program", pipeline.tts)
+        asr_program = websocket.args.get("asr_program", used_pipeline.asr)
+        assert asr_program, "Missing program for asr"
+
+        vad_program = websocket.args.get("vad_program", used_pipeline.vad)
+        assert vad_program, "Missing program for vad"
+
+        handle_program = websocket.args.get("handle_program", used_pipeline.handle)
+        assert handle_program, "Missing program for handle"
+
+        tts_program = websocket.args.get("tts_program", used_pipeline.tts)
         assert tts_program, "Missing program for tts"
-        _LOGGER.debug("speech-to-text: tts=%s", tts_program)
 
         in_rate = int(websocket.args.get("in_rate", DEFAULT_IN_RATE))
         in_width = int(websocket.args.get("in_width", DEFAULT_IN_WIDTH))
@@ -111,23 +121,82 @@ def add_pipeline(
         out_width = int(websocket.args.get("out_width", DEFAULT_OUT_WIDTH))
         out_channels = int(websocket.args.get("out_channels", DEFAULT_OUT_CHANNELS))
 
-        async def audio_stream():
+        # asr + vad
+        async with (
+            await create_process(rhasspy, ASR_DOMAIN, asr_program)
+        ) as asr_proc, (
+            await create_process(rhasspy, VAD_DOMAIN, vad_program)
+        ) as vad_proc:
+            assert asr_proc.stdin is not None
+            assert asr_proc.stdout is not None
+            assert vad_proc.stdin is not None
+            assert vad_proc.stdout is not None
+
+            mic_task = asyncio.create_task(websocket.receive())
+            vad_task = asyncio.create_task(async_read_event(vad_proc.stdout))
+            pending = {mic_task, vad_task}
+
             while True:
-                audio_bytes = await websocket.receive()
-                if not audio_bytes:
-                    yield bytes()
+                done, pending = await asyncio.wait(
+                    pending, return_when=asyncio.FIRST_COMPLETED
+                )
+
+                if mic_task in done:
+                    audio_bytes = mic_task.result()
+                    if isinstance(audio_bytes, bytes) and audio_bytes:
+                        mic_chunk = AudioChunk(
+                            in_rate, in_width, in_channels, audio_bytes
+                        )
+                        mic_chunk_event = mic_chunk.event()
+                        await asyncio.gather(
+                            async_write_event(mic_chunk_event, asr_proc.stdin),
+                            async_write_event(mic_chunk_event, vad_proc.stdin),
+                        )
+
+                    mic_task = asyncio.create_task(websocket.receive())
+                    pending.add(mic_task)
+
+                if vad_task in done:
+                    vad_event = vad_task.result()
+                    if vad_event is None:
+                        break
+
+                    if VoiceStarted.is_type(vad_event.type):
+                        _LOGGER.debug("stream-to-stream: voice started")
+                    elif VoiceStopped.is_type(vad_event.type):
+                        _LOGGER.debug("stream-to-stream: voice stopped")
+                        break
+
+                    vad_task = asyncio.create_task(async_read_event(vad_proc.stdout))
+                    pending.add(vad_task)
+
+            await websocket.send("stop")
+
+            # Get transcript from asr
+            await async_write_event(AudioStop().event(), asr_proc.stdin)
+            transcript: Optional[Transcript] = None
+            while True:
+                asr_event = await async_read_event(asr_proc.stdout)
+                if asr_event is None:
                     break
 
-                if isinstance(audio_bytes, bytes):
-                    yield audio_bytes
+                if Transcript.is_type(asr_event.type):
+                    transcript = Transcript.from_event(asr_event)
+                    _LOGGER.debug("stream-to-stream: asr=%s", transcript)
+                    break
 
-        transcript = await transcribe_stream(
-            rhasspy, asr_program, audio_stream(), in_rate, in_width, in_channels
-        )
+            handle_result: Optional[Union[Handled, NotHandled]] = None
+            if transcript is not None:
+                handle_result = await handle(rhasspy, handle_program, transcript)
+                _LOGGER.debug("stream-to-stream: handle=%s", handle_result)
 
-        # TODO: intent, handle
+            if (handle_result is not None) and handle_result.text:
+                _LOGGER.debug("stream-to-stream: sending tts")
+                converter = AudioChunkConverter(out_rate, out_width, out_channels)
+                async for tts_chunk in synthesize_stream(
+                    rhasspy, tts_program, handle_result.text
+                ):
+                    tts_chunk = converter.convert(tts_chunk)
+                    await websocket.send(tts_chunk.audio)
 
-        converter = AudioChunkConverter(out_rate, out_width, out_channels)
-        async for chunk in synthesize_stream(rhasspy, tts_program, transcript.text):
-            chunk = converter.convert(chunk)
-            await websocket.send(chunk.audio)
+                _LOGGER.debug("stream-to-stream: tts done")
