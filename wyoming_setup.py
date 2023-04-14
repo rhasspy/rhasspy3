d1bee26284d8d80f6c1f214095a22e4546e2030b
Michael Hansen <mike@rhasspy.org>
2023-04-14 11:50:36 -0500

    Wyoming package

---
+++ wyoming_setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import setuptools
+from setuptools import setup
+
+# -----------------------------------------------------------------------------
+
+setup(
+    name="wyoming",
+    version="0.0.1",
+    description="Protocol for Rhasspy Voice Assistant",
+    url="http://github.com/rhasspy/rhasspy3",
+    author="Michael Hansen",
+    author_email="mike@rhasspy.org",
+    license="MIT",
+    packages=["wyoming"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Text Processing :: Linguistic",
+        "License :: OSI Approved :: License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    keywords="voice assistant rhasspy",
+)
