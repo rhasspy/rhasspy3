5186eee2f4d460803f818e5c5ae6d1740c2ca20c
Michael Hansen <mike@rhasspy.org>
2023-05-10 15:40:20 -0500

    Add download hash check

--- programs/tts/piper/server/wyoming_piper/download.py
+++ programs/tts/piper/server/wyoming_piper/download.py
@@ -1,10 +1,16 @@
 """Utility for downloading Piper voices."""
+import logging
 import tarfile
 from pathlib import Path
-from typing import Optional, Union
+from typing import Dict, Optional, Union
 from urllib.request import urlopen
 
+from .file_hash import get_file_hash
+from .voice_hashes import VOICE_HASHES
+
 URL_FORMAT = "https://github.com/rhasspy/piper/releases/download/v0.0.2/voice-{voice_name}.tar.gz"
+
+_LOGGER = logging.getLogger(__name__)
 
 
 def download_voice(voice_name: str, dest_dir: Union[str, Path]) -> Path:
@@ -29,7 +35,51 @@ def find_voice(voice_name: str, dest_dir: Union[str, Path]) -> Optional[Path]:
     dest_dir = Path(dest_dir)
     voice_onnx = dest_dir / f"{voice_name}.onnx"
 
-    if voice_onnx.exists() and (voice_onnx.stat().st_size > 0):
+    expected_hash = VOICE_HASHES.get(voice_name)
+    if expected_hash is None:
+        # No expected hash, fall back to checking for a non-empty ONNX model
+        if voice_onnx.exists() and (voice_onnx.stat().st_size > 0):
+            return voice_onnx
+
+        return None
+
+    actual_hash: Dict[str, str] = {}
+    for hash_key in expected_hash:
+        file_to_hash = dest_dir / hash_key
+        if file_to_hash.exists():
+            actual_hash[hash_key] = get_file_hash(file_to_hash)
+        else:
+            # File is missing
+            actual_hash[hash_key] = ""
+
+    if actual_hash == expected_hash:
+        # Hashes match
         return voice_onnx
 
+    # Hashes do not match
+    _LOGGER.warning("Voice hashes do not match")
+    _LOGGER.warning("Expected: %s", expected_hash)
+    _LOGGER.warning("Got: %s", actual_hash)
+
     return None
+
+
+def get_voice_hash(model_dir: Union[str, Path]) -> Dict[str, str]:
+    """Get hashes for relevant voice files."""
+    model_dir = Path(model_dir)
+    files_to_hash = [
+        model_dir / "model.bin",
+        model_dir / "config.json",
+        model_dir / "vocabulary.txt",
+    ]
+
+    model_hash: Dict[str, str] = {}
+    for file_to_hash in files_to_hash:
+        hash_key = str(file_to_hash.relative_to(model_dir))
+        if file_to_hash.exists():
+            model_hash[hash_key] = get_file_hash(file_to_hash)
+        else:
+            # File is missing
+            model_hash[hash_key] = ""
+
+    return model_hash
---
+++ programs/tts/piper/server/wyoming_piper/voice_hashes.py
@@ -0,0 +1,208 @@
+# Generated from Piper
+
+VOICE_HASHES = {
+    "ca-upc_ona-x-low": {
+        "ca-upc_ona-x-low.onnx": "ca22734cd8c5b01dd1fefbb42067ab06",
+        "ca-upc_ona-x-low.onnx.json": "44952d2434e8759aed8e780c6ae695fa",
+    },
+    "ca-upc_pau-x-low": {
+        "ca-upc_pau-x-low.onnx": "504e8a643d5284fbfc95e9e392288b86",
+        "ca-upc_pau-x-low.onnx.json": "44952d2434e8759aed8e780c6ae695fa",
+    },
+    "da-nst_talesyntese-medium": {
+        "da-nst_talesyntese-medium.onnx.json": "577b2922d5d4db2dc1d19605c5504ca8",
+        "da-nst_talesyntese-medium.onnx": "fc70e089bf65ac2c492bb8204f7541c0",
+    },
+    "de-eva_k-x-low": {
+        "de-eva_k-x-low.onnx.json": "1a46002d8bbef555078530e0953e61f9",
+        "de-eva_k-x-low.onnx": "51bfc52a58282c2e4fc01ae66567a708",
+    },
+    "de-karlsson-low": {
+        "de-karlsson-low.onnx.json": "1a46002d8bbef555078530e0953e61f9",
+        "de-karlsson-low.onnx": "c94b5b8e8c7147b4b2c4a19ca5a3c41b",
+    },
+    "de-kerstin-low": {
+        "de-kerstin-low.onnx.json": "1a46002d8bbef555078530e0953e61f9",
+        "de-kerstin-low.onnx": "1d5e5788cfddb04cbb34418f2841931e",
+    },
+    "de-pavoque-low": {
+        "de-pavoque-low.onnx.json": "1a46002d8bbef555078530e0953e61f9",
+        "de-pavoque-low.onnx": "bc37dccbad87fd65c8501c412c0c31ca",
+    },
+    "de-ramona-low": {
+        "de-ramona-low.onnx.json": "1a46002d8bbef555078530e0953e61f9",
+        "de-ramona-low.onnx": "b4aaf3673170a0d96519cdc992c23fda",
+    },
+    "de-thorsten-low": {
+        "de-thorsten-low.onnx.json": "1a46002d8bbef555078530e0953e61f9",
+        "de-thorsten-low.onnx": "c06eb96aceb61895fcb09ffc30eef60b",
+    },
+    "el-gr-rapunzelina-low": {
+        "el-gr-rapunzelina-low.onnx.json": "72e6d38fb08d93b9858f246653b63f88",
+        "el-gr-rapunzelina-low.onnx": "04e0151b653bb64540b1cde027054140",
+    },
+    "en-gb-alan-low": {
+        "en-gb-alan-low.onnx.json": "9c7dc3e4f83524944b196c1646dc7fd6",
+        "en-gb-alan-low.onnx": "2acae8c79395ab109a7572f0afa61fff",
+    },
+    "en-gb-southern_english_female-low": {
+        "en-gb-southern_english_female-low.onnx": "596c7ed4d8488cf64e027765dce2dad1",
+        "en-gb-southern_english_female-low.onnx.json": "9c7dc3e4f83524944b196c1646dc7fd6",
+    },
+    "en-us-amy-low": {
+        "en-us-amy-low.onnx.json": "06b7255db7f49069a9abcb3037804cc5",
+        "en-us-amy-low.onnx": "3c3f6a6ec605f3a59763256d3b2db012",
+    },
+    "en-us-danny-low": {
+        "en-us-danny-low.onnx": "73cc296e178ab3d2a5698179b629cd12",
+        "en-us-danny-low.onnx.json": "06b7255db7f49069a9abcb3037804cc5",
+    },
+    "en-us-kathleen-low": {
+        "en-us-kathleen-low.onnx": "dd1ab131724b1cff76fe388252bec47b",
+        "en-us-kathleen-low.onnx.json": "06b7255db7f49069a9abcb3037804cc5",
+    },
+    "en-us-lessac-low": {
+        "en-us-lessac-low.onnx": "ed56295e5ac15e47d3d3cfec1b56eb8f",
+        "en-us-lessac-low.onnx.json": "06b7255db7f49069a9abcb3037804cc5",
+    },
+    "en-us-lessac-medium": {
+        "en-us-lessac-medium.onnx.json": "06b7255db7f49069a9abcb3037804cc5",
+        "en-us-lessac-medium.onnx": "a95c5f64961a45c36bb41db746b4c04c",
+    },
+    "en-us-libritts-high": {
+        "en-us-libritts-high.onnx": "61d7845257f8abdc27476f606151ef8d",
+        "en-us-libritts-high.onnx.json": "ffab55eb698798ef8fcce3dc47bbcb26",
+    },
+    "en-us-ryan-high": {
+        "en-us-ryan-high.onnx.json": "6023490bab8a15da7e59bb9fd3c29762",
+        "en-us-ryan-high.onnx": "5d879a17bddf5007f76655b445ba78b4",
+    },
+    "en-us-ryan-low": {
+        "en-us-ryan-low.onnx.json": "9cacb6725a72074808d677454d046cff",
+        "en-us-ryan-low.onnx": "32f6a995d6d561cd040b20a76f4edb1e",
+    },
+    "en-us-ryan-medium": {
+        "en-us-ryan-medium.onnx.json": "6023490bab8a15da7e59bb9fd3c29762",
+        "en-us-ryan-medium.onnx": "14dfbc6c0d4ca48f78d1ed2fa60fe6a6",
+    },
+    "es-carlfm-x-low": {
+        "es-carlfm-x-low.onnx": "4137b5aee01ea6241080fc4dbe59a8ee",
+        "es-carlfm-x-low.onnx.json": "a3750f9fff6ae97bc60de668c8107e5f",
+    },
+    "es-mls_10246-low": {
+        "es-mls_10246-low.onnx": "ab8e93c9d2714fd4481fbca4e2a38891",
+        "es-mls_10246-low.onnx.json": "a3750f9fff6ae97bc60de668c8107e5f",
+    },
+    "es-mls_9972-low": {
+        "es-mls_9972-low.onnx.json": "a3750f9fff6ae97bc60de668c8107e5f",
+        "es-mls_9972-low.onnx": "587f2fc38dc3f582e771c3748465e2a2",
+    },
+    "fi-harri-low": {
+        "fi-harri-low.onnx.json": "eba0de0dea4f2adb0cb5c4bcc3f636bc",
+        "fi-harri-low.onnx": "f44b67203de7fd488eabc4692d30b598",
+    },
+    "fr-gilles-low": {
+        "fr-gilles-low.onnx": "f984386d1f0927597f09a3ec10b11b5d",
+        "fr-gilles-low.onnx.json": "c800dbf8a43eb67a1ec4d5438893c11c",
+    },
+    "fr-mls_1840-low": {
+        "fr-mls_1840-low.onnx.json": "c800dbf8a43eb67a1ec4d5438893c11c",
+        "fr-mls_1840-low.onnx": "1873b5d95cb0aad9909d32d1747ae72b",
+    },
+    "fr-siwis-low": {
+        "fr-siwis-low.onnx": "fcb614122005d70f27e4e61e58b4bb56",
+        "fr-siwis-low.onnx.json": "c800dbf8a43eb67a1ec4d5438893c11c",
+    },
+    "fr-siwis-medium": {
+        "fr-siwis-medium.onnx.json": "4fe976ffdd579bbe78ae59939a2eb0b4",
+        "fr-siwis-medium.onnx": "9b545ff8882370d99c8e7147c59e3788",
+    },
+    "is-bui-medium": {
+        "is-bui-medium.onnx.json": "fe40a8dc0810d54d53f526ea9b97fac0",
+        "is-bui-medium.onnx": "08332bb41a67b52a3361bd1e8e36fb10",
+    },
+    "is-salka-medium": {
+        "is-salka-medium.onnx": "5967c9456b931d6123687d7b78fd81a7",
+        "is-salka-medium.onnx.json": "fe40a8dc0810d54d53f526ea9b97fac0",
+    },
+    "is-steinn-medium": {
+        "is-steinn-medium.onnx": "fd8189eb0a72e78d525e70a71aaa792c",
+        "is-steinn-medium.onnx.json": "fe40a8dc0810d54d53f526ea9b97fac0",
+    },
+    "is-ugla-medium": {
+        "is-ugla-medium.onnx": "722fcea3546f0113ad6664290aa97cab",
+        "is-ugla-medium.onnx.json": "fe40a8dc0810d54d53f526ea9b97fac0",
+    },
+    "it-riccardo_fasol-x-low": {
+        "it-riccardo_fasol-x-low.onnx": "2c564b67f6bfaf3ad02d28ab528929b8",
+        "it-riccardo_fasol-x-low.onnx.json": "2216aa093b516a5f07b7634f4c7fda1f",
+    },
+    "kk-iseke-x-low": {
+        "kk-iseke-x-low.onnx": "1674f3f4ce48981d77e500741afa4ff9",
+        "kk-iseke-x-low.onnx.json": "838be60c9b2760337eb8e54a671f317b",
+    },
+    "kk-issai-high": {
+        "kk-issai-high.onnx": "d5a97c25feb0949c187ae5f8e72753e3",
+        "kk-issai-high.onnx.json": "5fc3ec6aee3d104c00a3e2ee0914e9d5",
+    },
+    "kk-raya-x-low": {
+        "kk-raya-x-low.onnx": "476ecc32e07cad26572a50f26d0ebe28",
+        "kk-raya-x-low.onnx.json": "838be60c9b2760337eb8e54a671f317b",
+    },
+    "ne-google-medium": {
+        "ne-google-medium.onnx.json": "309124f6068864743fba1c021cb599f4",
+        "ne-google-medium.onnx": "5d2f63b345cb6abd125a67a14b4e2553",
+    },
+    "ne-google-x-low": {
+        "ne-google-x-low.onnx.json": "309124f6068864743fba1c021cb599f4",
+        "ne-google-x-low.onnx": "b11030daccc781a7db64c9413197ca8a",
+    },
+    "nl-mls_5809-low": {
+        "nl-mls_5809-low.onnx": "e69130a776b04c9962a1fefb4878d7d9",
+        "nl-mls_5809-low.onnx.json": "189e6fd4e40bf8322800f50a743b4778",
+    },
+    "nl-mls_7432-low": {
+        "nl-mls_7432-low.onnx.json": "189e6fd4e40bf8322800f50a743b4778",
+        "nl-mls_7432-low.onnx": "044b69d583e191203997761434607273",
+    },
+    "nl-nathalie-x-low": {
+        "nl-nathalie-x-low.onnx.json": "189e6fd4e40bf8322800f50a743b4778",
+        "nl-nathalie-x-low.onnx": "4a00803b60caecad30ea612bcd9f9344",
+    },
+    "nl-rdh-medium": {
+        "nl-rdh-medium.onnx.json": "eaae3e21dcd4899d5b522067ec994fd9",
+        "nl-rdh-medium.onnx": "33d3469d745677ec4d7e96eb4145b09e",
+    },
+    "nl-rdh-x-low": {
+        "nl-rdh-x-low.onnx": "7d60d0de9ad9ec11a1d293665743afda",
+        "nl-rdh-x-low.onnx.json": "189e6fd4e40bf8322800f50a743b4778",
+    },
+    "no-talesyntese-medium": {
+        "no-talesyntese-medium.onnx.json": "67187aa5313d148c65ca24a096e3ee82",
+        "no-talesyntese-medium.onnx": "b82ce22dc75bc830954551e4c214490c",
+    },
+    "pl-mls_6892-low": {
+        "pl-mls_6892-low.onnx.json": "ea661adb29682d6900dbfbd76e3f71bd",
+        "pl-mls_6892-low.onnx": "8590d8e979292ca35d20e6e123bfa612",
+    },
+    "pt-br-edresson-low": {
+        "pt-br-edresson-low.onnx": "53e365c040dd07890fe1855b64c7cc58",
+        "pt-br-edresson-low.onnx.json": "3ec806d00312e12c21743eb88e8c7c81",
+    },
+    "uk-lada-x-low": {
+        "uk-lada-x-low.onnx.json": "0dc53021ef5b2086b9072b1f5ad90b18",
+        "uk-lada-x-low.onnx": "b84110e3923d64cdd4e0056a22090557",
+    },
+    "vi-25hours-single-low": {
+        "vi-25hours-single-low.onnx": "54ff8fb35b0084336377ddd10717e1fa",
+        "vi-25hours-single-low.onnx.json": "9af5281977bb1d9d06297d4585c38597",
+    },
+    "vi-vivos-x-low": {
+        "vi-vivos-x-low.onnx.json": "c578b42f2c512d8011c842a12af23134",
+        "vi-vivos-x-low.onnx": "d5880d32e340f57489dcb9d4f1f7aa04",
+    },
+    "zh-cn-huayan-x-low": {
+        "zh-cn-huayan-x-low.onnx.json": "b086dd6808691b3ba114645ab225f24b",
+        "zh-cn-huayan-x-low.onnx": "2b96570db6becd09814a608c8d14a64f",
+    },
+}
