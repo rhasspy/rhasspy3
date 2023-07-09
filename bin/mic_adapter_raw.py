339a439f2a227a99d5dc7b2d366fd38fcc58208b
Michael Hansen <mike@rhasspy.org>
2023-07-09 15:26:39 -0500

    Async mic adapter

--- bin/mic_adapter_raw.py
+++ bin/mic_adapter_raw.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
 """Reads raw audio chunks from stdin."""
 import argparse
-import contextlib
+import asyncio
 import logging
 import shlex
-import subprocess
-import threading
 import time
 from pathlib import Path
-from typing import Union
+from typing import Optional
 
 from rhasspy3.audio import DEFAULT_SAMPLES_PER_CHUNK, AudioChunk, AudioStart, AudioStop
-from rhasspy3.event import read_event, write_event
+from rhasspy3.event import async_get_stdin, async_read_event, write_event
 
 _FILE = Path(__file__)
 _DIR = _FILE.parent
 _LOGGER = logging.getLogger(_FILE.stem)
 
 
-def main() -> None:
+async def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "command",
@@ -62,53 +60,81 @@ def main() -> None:
     bytes_per_chunk = args.samples_per_chunk * args.width * args.channels
 
     if args.shell:
-        command = args.command
-    else:
-        command = shlex.split(args.command)
-
-    proc = subprocess.Popen(command, stdout=subprocess.PIPE)
-
-    has_filter = bool(args.filter)
-    if has_filter:
-        filter_proc: Union[
-            subprocess.Popen, contextlib.AbstractContextManager
-        ] = subprocess.Popen(
-            shlex.split(args.filter),
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.DEVNULL,
+        proc = await asyncio.create_subprocess_shell(
+            args.command,
+            stdout=asyncio.subprocess.PIPE,
         )
-        assert isinstance(filter_proc, subprocess.Popen)
+    else:
+        program, *program_args = shlex.split(args.command)
+        proc = await asyncio.create_subprocess_exec(
+            program,
+            *program_args,
+            stdout=asyncio.subprocess.PIPE,
+        )
+
+    assert proc.stdout is not None
+
+    # Filter for raw input audio.
+    # The program must return the same number of bytes for each chunk.
+    filter_proc: Optional[asyncio.subprocess.Process] = None
+    if args.filter:
+        filter_program, *filter_args = shlex.split(args.filter)
+        _LOGGER.debug(
+            "Running filter: program=%s, args=%s", filter_program, filter_args
+        )
+        filter_proc = await asyncio.create_subprocess_exec(
+            filter_program,
+            *filter_args,
+            stdin=asyncio.subprocess.PIPE,
+            stdout=asyncio.subprocess.PIPE,
+        )
         assert filter_proc.stdin is not None
         assert filter_proc.stdout is not None
-    else:
-        filter_proc = contextlib.nullcontext()
 
-    with proc, filter_proc:
-        assert proc.stdout is not None
+        filter_proc.stdin.write(bytes(2048))
+        await filter_proc.stdin.drain()
+        await filter_proc.stdout.readexactly(2048)
 
-        stop_event = threading.Event()
-        threading.Thread(target=wait_for_stop, daemon=True, args=(stop_event,)).start()
+    stdin_reader = await async_get_stdin()
+    stdin_task = asyncio.create_task(async_read_event(stdin_reader))
+    proc_task = asyncio.create_task(proc.stdout.readexactly(bytes_per_chunk))
+    pending = {stdin_task, proc_task}
 
-        write_event(
-            AudioStart(
-                args.rate, args.width, args.channels, timestamp=time.monotonic_ns()
-            ).event()
-        )
+    write_event(
+        AudioStart(
+            args.rate, args.width, args.channels, timestamp=time.monotonic_ns()
+        ).event()
+    )
 
-        try:
-            while True:
-                if stop_event.is_set():
+    try:
+        while True:
+            done, pending = await asyncio.wait(
+                pending, return_when=asyncio.FIRST_COMPLETED
+            )
+
+            if stdin_task in done:
+                # Event from stdin
+                event = stdin_task.result()
+                if event is None:
                     break
 
-                audio_bytes = proc.stdout.read(bytes_per_chunk)
+                if AudioStop.is_type(event.type):
+                    break
+
+                # Next stdin event
+                stdin_task = asyncio.create_task(async_read_event(stdin_reader))
+                pending.add(stdin_task)
+
+            if proc_task in done:
+                # Audio chunk from mic process
+                audio_bytes = proc_task.result()
                 if not audio_bytes:
                     break
 
-                if has_filter:
+                if filter_proc is not None:
                     filter_proc.stdin.write(audio_bytes)
-                    filter_proc.stdin.flush()
-                    audio_bytes = filter_proc.stdout.read(len(audio_bytes))
+                    await filter_proc.stdin.drain()
+                    audio_bytes = await filter_proc.stdout.readexactly(len(audio_bytes))
 
                 write_event(
                     AudioChunk(
@@ -119,25 +145,34 @@ def main() -> None:
                         timestamp=time.monotonic_ns(),
                     ).event()
                 )
-        except KeyboardInterrupt:
-            pass
 
+                # Next chunk
+                proc_task = asyncio.create_task(
+                    proc.stdout.readexactly(bytes_per_chunk)
+                )
+                pending.add(proc_task)
+    finally:
+        write_event(AudioStop().event())
 
-def wait_for_stop(stop_event: threading.Event):
-    try:
-        while True:
-            event = read_event()
-            if event is None:
-                break
+        for task in pending:
+            task.cancel()
 
-            if AudioStop.is_type(event.type):
-                stop_event.set()
-                break
-    except KeyboardInterrupt:
-        pass
-    except Exception:
-        _LOGGER.exception("Unexpected error in wait_for_stop")
+        if proc.returncode is None:
+            _LOGGER.debug("Stopping %s", args.command)
+            proc.terminate()
+            await proc.wait()
+
+        if (filter_proc is not None) and (filter_proc.returncode is None):
+            # Stop filter
+            _LOGGER.debug("Stopping %s", args.filter)
+            filter_proc.stdin.close()
+            await filter_proc.wait()
+
+    _LOGGER.info("Stopped")
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
